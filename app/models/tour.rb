348e7dc6c6fab97b83e6287d64abb3682372ceb1
chinhnc <chinhharubn@gmail.com>
2016-12-16 21:39:29 +0700

    update template for admin

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -15,6 +15,9 @@ class Tour < ApplicationRecord
   validates :start_date, presence: true,
     date: { after_or_equal_to: Proc.new { Date.today },
     message: "must be at least #{(Date.today + 1).to_s}" }, on: :create
+  validates :start_place_id, presence: true
+  validates :end_place_id, presence: true
+  validates :category_id, presence: true
   # validate :check_discount
 
   # def check_discount
